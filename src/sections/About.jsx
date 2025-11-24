aec499398516cfbcfc9ced5c603631b117fa886a
Andrez18 <ieca.andresgomez@gmail.com>
2025-11-24 13:16:17 -0500

    Actualizar texto del botón en el componente CTA y mejorar el formato del texto en la sección About

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -35,7 +35,16 @@ const About = () => {
     <>
       <div ref={aboutRef} className='h-screen bg-white rounded-tl-[60px] rounded-tr-[60px] relative z-10'>
           {/* Text container */}
-          <div className='about-text main-container py-4 lg:py-12 h-full flex justify-center items-center font-heading text-black text-2xl md:text-3xl xl:text-[40px] 2xl:text-5xl leading-[1.25]'>Mi enfoque combina un diseño limpio, interacciones fluidas y detalles bien pensados ​​para garantizar que cada proyecto sea visualmente impactante y altamente funcional. Ya sea diseñar un portafolio, crear un sitio web empresarial o fortalecer la presencia online de una marca, me centro en crear trabajos que no solo sean atractivos, sino que también conecten con las personas de forma significativa.</div>
+        <div 
+          className='about-text main-container py-4 lg:py-12 font-heading text-black 
+                    text-2xl md:text-3xl xl:text-[40px] 2xl:text-5xl leading-relaxed 
+                    max-w-8xl mx-auto text-center'>
+          Mi enfoque combina un diseño limpio, interacciones fluidas y detalles bien pensados 
+          para garantizar que cada proyecto sea visualmente impactante y altamente funcional. 
+          Ya sea diseñar un portafolio, crear un sitio web empresarial o fortalecer la presencia 
+          online de una marca, me centro en crear trabajos que no solo sean atractivos, sino que 
+          también conecten con las personas de forma significativa.
+        </div>
       </div>
     </>
   )
